d7d0889e6380dd659df1fde036531c6b1e0d2cac
Osvaldo Muguercia <elosvadel84@gmail.com>
2024-04-10 12:13:44 -0400

    Refactor Footer component and styles to set Active style depend of page rendered and usertype

--- ROCKET_FOOD/components/Footer.js
+++ ROCKET_FOOD/components/Footer.js
@@ -2,22 +2,23 @@ import React, { useContext } from "react";
 import { View, TouchableOpacity, Text } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
 import styles from "../styles/FooterStyle";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
 import AuthContext from "../services/AuthContext";
 const Footer = () => {
   const navigation = useNavigation();
   const { userType } = useContext(AuthContext);
+  const route = useRoute();
 
   return (
     <View style={styles.container}>
   {userType === 'customer' && (
     <View style={styles.buttonContainer}>
       <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.navigate("Restaurants")}
-      >
-        <Icon name="cutlery" size={30} color="#000" />
-      </TouchableOpacity>
+    style={[styles.button, route.name === 'Restaurants' && styles.activeButton]}
+    onPress={() => navigation.navigate('Restaurants')}
+  >
+    <Icon name="cutlery" size={30} color="#000" />
+  </TouchableOpacity>
       <Text style={styles.buttonText}>Restaurants</Text>
     </View>
   )}
@@ -25,11 +26,11 @@ const Footer = () => {
   {userType === 'customer' && (
     <View style={styles.buttonContainer}>
       <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.navigate("Orders")}
-      >
-        <Icon name="history" size={30} color="#000" />
-      </TouchableOpacity>
+    style={[styles.button, route.name === 'Orders' && styles.activeButton]}
+    onPress={() => navigation.navigate('Orders')}
+  >
+    <Icon name="history" size={30} color="#000" />
+  </TouchableOpacity>
       <Text style={styles.buttonText}>Order History</Text>
     </View>
   )}
@@ -37,22 +38,22 @@ const Footer = () => {
   {userType === 'courier' && (
     <View style={styles.buttonContainer}>
       <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.navigate("CourierDeliveries")}
-      >
-        <Icon name="truck" size={30} color="#000" />
-      </TouchableOpacity>
+    style={[styles.button, route.name === 'CourierDeliveries' && styles.activeButton]}
+    onPress={() => navigation.navigate('CourierDeliveries')}
+  >
+    <Icon name="truck" size={30} color="#000" />
+  </TouchableOpacity>
       <Text style={styles.buttonText}>Deliveries</Text>
     </View>
   )}
 
   <View style={styles.buttonContainer}>
-    <TouchableOpacity
-      style={styles.button}
-      onPress={() => navigation.navigate("Account")}
-    >
-      <Icon name="user" size={30} color="#000" />
-    </TouchableOpacity>
+  <TouchableOpacity
+    style={[styles.button, (route.name === 'CustomerAccount' || route.name === 'CourierAccount') && styles.activeButton]}
+    onPress={() => navigation.navigate(userType === 'customer' ? 'CustomerAccount' : 'CourierAccount')}
+  >
+    <Icon name="user" size={30} color="#000" />
+  </TouchableOpacity>
     <Text style={styles.buttonText}>Account</Text>
   </View>
 </View>
