909a871e4e31c79a528c1223cbd744b18f4f5684
Osvaldo Muguercia <elosvadel84@gmail.com>
2024-04-10 11:54:11 -0400

    Refactor Footer component to conditionally render buttons based on user type

--- ROCKET_FOOD/components/Footer.js
+++ ROCKET_FOOD/components/Footer.js
@@ -1,8 +1,8 @@
 import React, { useContext } from "react";
 import { View, TouchableOpacity, Text } from "react-native";
-import Icon from 'react-native-vector-icons/FontAwesome';
+import Icon from "react-native-vector-icons/FontAwesome";
 import styles from "../styles/FooterStyle";
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation } from "@react-navigation/native";
 import AuthContext from "../services/AuthContext";
 const Footer = () => {
   const navigation = useNavigation();
@@ -10,28 +10,52 @@ const Footer = () => {
 
   return (
     <View style={styles.container}>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Restaurants')}>
-          <Icon name="cutlery" size={30} color="#000" />
-        </TouchableOpacity>
-        <Text style={styles.buttonText}>Restaurants</Text>
-      </View>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Orders')}>
-          <Icon name="history" size={30} color="#000" />
-        </TouchableOpacity>
-        <Text style={styles.buttonText}>Order History</Text>
-      </View>
-      <View style={styles.buttonContainer}>
-      <TouchableOpacity 
-          style={styles.button} 
-          onPress={() => navigation.navigate(userType === 'courier' ? 'CourierAccount' : 'CustomerAccount')}
-        >
-          <Icon name="user" size={30} color="#000" />
-        </TouchableOpacity>
-        <Text style={styles.buttonText}>Account</Text>
-      </View>
+  {userType === 'customer' && (
+    <View style={styles.buttonContainer}>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate("Restaurants")}
+      >
+        <Icon name="cutlery" size={30} color="#000" />
+      </TouchableOpacity>
+      <Text style={styles.buttonText}>Restaurants</Text>
     </View>
+  )}
+
+  {userType === 'customer' && (
+    <View style={styles.buttonContainer}>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate("Orders")}
+      >
+        <Icon name="history" size={30} color="#000" />
+      </TouchableOpacity>
+      <Text style={styles.buttonText}>Order History</Text>
+    </View>
+  )}
+
+  {userType === 'courier' && (
+    <View style={styles.buttonContainer}>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate("CourierDeliveries")}
+      >
+        <Icon name="truck" size={30} color="#000" />
+      </TouchableOpacity>
+      <Text style={styles.buttonText}>Deliveries</Text>
+    </View>
+  )}
+
+  <View style={styles.buttonContainer}>
+    <TouchableOpacity
+      style={styles.button}
+      onPress={() => navigation.navigate("Account")}
+    >
+      <Icon name="user" size={30} color="#000" />
+    </TouchableOpacity>
+    <Text style={styles.buttonText}>Account</Text>
+  </View>
+</View>
   );
 };
 
